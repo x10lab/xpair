e0c1c15ede6a4f255658842296710831d7c5f0d3
GwanHyeong Lyu <bestgoodtime07@gmail.com>
2026-06-15 15:16:05 +0900

    Squashed 'client/ide/vendor/vscodium/' content from commit eb967d7
    git-subtree-dir: client/ide/vendor/vscodium
    git-subtree-split: eb967d7bffbcbaec6f9aa89ef7515b3cfe46dd0b

---
+++ build/linux/ppc64le/electron.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export ELECTRON_VERSION="41.0.3"
+export VSCODE_ELECTRON_TAG="v${ELECTRON_VERSION}"
---
+++ build/windows/package.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+
+set -ex
+
+if [[ "${CI_BUILD}" == "no" ]]; then
+  exit 1
+fi
+
+tar -xzf ./vscode.tar.gz
+
+cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+for i in {1..5}; do # try 5 times
+  npm ci && break
+  if [[ $i == 5 ]]; then
+    echo "Npm install failed too many times" >&2
+    exit 1
+  fi
+  echo "Npm install failed $i, trying again..."
+done
+
+node build/azure-pipelines/distro/mixin-npm.ts
+
+# delete native files built in the `compile` step
+find .build/extensions -type f -name '*.node' -print -delete
+
+. ../build/windows/rtf/make.sh
+
+# generate Group Policy definitions
+npm run copy-policy-dto --prefix build
+node build/lib/policies/policyGenerator.ts build/lib/policies/policyData.jsonc win32
+
+# node build/win32/explorer-dll-fetcher.ts .build/win32/appx
+
+npm run gulp "vscode-win32-${VSCODE_ARCH}-min-packing"
+
+. ../build_cli.sh
+
+if [[ "${VSCODE_ARCH}" == "x64" ]]; then
+  if [[ "${SHOULD_BUILD_REH}" != "no" ]]; then
+    echo "Building REH"
+    npm run gulp minify-vscode-reh
+    npm run gulp "vscode-reh-win32-${VSCODE_ARCH}-min-ci"
+  fi
+
+  if [[ "${SHOULD_BUILD_REH_WEB}" != "no" ]]; then
+    echo "Building REH-web"
+    npm run gulp minify-vscode-reh-web
+    npm run gulp "vscode-reh-web-win32-${VSCODE_ARCH}-min-ci"
+  fi
+fi
+
+cd ..
